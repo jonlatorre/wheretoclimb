176c784e47bbd347de874917664b396a18be3766
Jon Latorre <moebius@etxea.net>
2012-11-13 19:28:41 +0100

    Adjust theme width and home url

--- urls.py
+++ urls.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.conf.urls.defaults import *
-from django.views.generic.simple import direct_to_template
+from django.views.generic.simple import direct_to_template, redirect_to
 
 from django.contrib import admin
 admin.autodiscover()
@@ -14,9 +14,10 @@ handler500 = "pinax.views.server_error"
 
 
 urlpatterns = patterns("",
-    url(r"^$", direct_to_template, {
-        "template": "homepage.html",
-    }, name="home"),
+#    url(r"^$", direct_to_template, {
+#        "template": "homepage.html",
+#    }, name="home"),
+    url(r"^$", redirect_to, {'url':'/crags/'}, name="home"),
     url(r"^admin/invite_user/$", "pinax.apps.signup_codes.views.admin_invite_user", name="admin_invite_user"),
     url(dajaxice_config.dajaxice_url, include('dajaxice.urls')),
     url(r"^admin/", include(admin.site.urls)),
