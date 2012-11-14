9f51ff8d86a8ad452640ef6c9b85c46fc2cf72fc
Jon Latorre <moebius@etxea.net>
2012-11-14 10:52:11 +0100

    Servinf media files en devel enviroment

--- urls.py
+++ urls.py
@@ -37,3 +37,13 @@ if settings.SERVE_MEDIA:
     urlpatterns += patterns("",
         url(r"", include("staticfiles.urls")),
     )
+if settings.DEBUG:
+    from django.views.static import serve
+    _media_url = settings.MEDIA_URL
+    if _media_url.startswith('/'):
+        _media_url = _media_url[1:]
+        urlpatterns += patterns('',
+                                (r'^%s(?P<path>.*)$' % _media_url,
+                                serve,
+                                {'document_root': settings.MEDIA_ROOT}))
+    del(_media_url, serve)
